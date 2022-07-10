43c40166b2977997e3509a2ec6393b582d6e3e12
Makar Glavanar <mr.glavanar@bk.ru>
2022-07-11 03:35:41 +0400

    npm release verion 2.0.0-beta-1657496141673

--- dist/db/drivers/mongo/adapter/CollectionAdapter.d.ts
+++ dist/db/drivers/mongo/adapter/CollectionAdapter.d.ts
@@ -15,12 +15,12 @@ export declare class CollectionAdapter<T, R extends RecordSchema<T>, F extends P
     getMany(query: Filter<R>, options?: FindOptions<R>): Promise<T[]>;
     getAll(options?: FindOptions<R>): Promise<T[]>;
     getDBRecordField<K extends keyof WithId<R>>(query: Filter<R>, fieldName: K, options?: FindOptions<R>): Promise<WithId<R>[K]>;
-    getRecordsCount(query: Filter<R>, options: CountDocumentsOptions): Promise<number>;
+    getRecordsCount(query: Filter<R>, options?: CountDocumentsOptions): Promise<number>;
     updateOne(query: Filter<R>, updates: UpdateFilter<R> | Partial<R>, options?: UpdateOptions): Promise<boolean>;
     updateMany(query: Filter<R>, updates: UpdateFilter<R> | Partial<R>, options?: UpdateOptions): Promise<boolean>;
     deleteOne(query: Filter<R>, options?: DeleteOptions): Promise<boolean>;
-    deleteMany(query: Filter<R>, options: DeleteOptions): Promise<boolean>;
-    aggregate(pipeline: Parameters<Collection<R>['aggregate']>[0], options: Parameters<Collection<R>['aggregate']>[1]): Promise<T[]>;
+    deleteMany(query: Filter<R>, options?: DeleteOptions): Promise<boolean>;
+    aggregate(pipeline: Parameters<Collection<R>['aggregate']>[0], options?: Parameters<Collection<R>['aggregate']>[1]): Promise<T[]>;
     createIndex(fieldOrSpec: IndexSpecification, options?: CreateIndexesOptions): Promise<void>;
     private getDBCollection;
     private createEntityFromDBRecord;
